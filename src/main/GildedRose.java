c7c94cc31bf36c255259f39693bf542140590cd0
Kseniia <smolnikova.ksenia@gmail.com>
2024-11-13 21:25:28 +0200

    Modified UpdateQuality function

--- src/main/GildedRose.java
+++ src/main/GildedRose.java
@@ -70,33 +70,24 @@ class GildedRose {
     }
 }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-
-            if (items[i].quality > 0 && !items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert" && !items[i].name.equals("Sulfuras, Hand of Ragnaros"))
-            items[i].sellIn = items[i].sellIn - 1;
-            if (items[i].sellIn < 0) {
-                items[i].quality = items[i].quality - 2;
-            }
-            if (items[i].sellIn > 0){
-                items[i].quality = items[i].quality - 1;
-            }
-
-
-            if ((items[i].name.equals("Aged Brie") && items[i].quality < 50) || (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].sellIn < 11 && items[i].quality < 50)){
-                items[i].quality = items[i].quality + 1;
-            }
-
-
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                items[i].quality = 0;
-            }
-
-            if (items[i].name.equals("Aged Brie") && (items[i].quality < 50)) {
-                items[i].quality = items[i].quality + 1;
-
-            }
+public void updateQuality() {
+    for (Item item : items) {
+        if (isSulfuras(item)) {
+            continue; // Skip Sulfuras items as their quality does not change
         }
+
+        decreaseSellIn(item);
+
+        if (isAgedBrie(item)) {
+            updateAgedBrieQuality(item);
+            continue;
+        }
+
+        if (isBackstagePass(item)) {
+            updateBackstagePassQuality(item);
+            continue;
+        }
+
+        updateStandardItemQuality(item);
     }
 }
-
