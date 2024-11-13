a9f163b3804f6bcf9d1b088245b31ab9cf3d58f6
Kseniia <smolnikova.ksenia@gmail.com>
2024-11-13 22:10:55 +0200

    Now each item type has its own updater class, which implements the ItemUpdater interface. This reduces the size of each class like it was needed for good refactoring

--- src/main/GildedRose.java
+++ src/main/GildedRose.java
@@ -1,52 +1,73 @@
 package com.gildedrose;
 
-class GildedRose {
-    Item[] items;
+import java.util.List;
 
-    public GildedRose(Item[] items) {
+class Item {
+    String name;
+    int sellIn;
+    int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+}
+
+interface ItemUpdater {
+    void update(Item item);
+}
+
+class GildedRose {
+    private List<Item> items;
+
+    public GildedRose(List<Item> items) {
         this.items = items;
     }
 
-    private boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    public void updateQuality() {
+        for (Item item : items) {
+            ItemUpdater updater = ItemUpdaterFactory.getUpdater(item);
+            updater.update(item);
+        }
     }
+}
 
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
+class ItemUpdaterFactory {
+    public static ItemUpdater getUpdater(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                return new AgedBrieUpdater();
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return new BackstagePassUpdater();
+            case "Sulfuras, Hand of Ragnaros":
+                return new SulfurasUpdater();
+            default:
+                return new StandardItemUpdater();
+        }
     }
+}
 
-    private boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isExpired(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private void decreaseSellIn(Item item) {
-        item.sellIn --;
+class AgedBrieUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
+        increaseQuality(item);
+        if (item.sellIn < 0) {
+            increaseQuality(item);
+        }
+        item.sellIn--;
     }
 
     private void increaseQuality(Item item) {
         if (item.quality < 50) {
-            item.quality ++;
+            item.quality++;
         }
     }
+}
 
-    private void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality --;
-        }
-    }
-
-    private void updateAgedBrieQuality(Item item) {
-        increaseQuality(item);
-        if (isExpired(item)) {
-            increaseQuality(item);
-        }
-    }
-
-    private void updateBackstagePassQuality(Item item) {
+class BackstagePassUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
         increaseQuality(item);
 
         if (item.sellIn < 10) {
@@ -56,38 +77,40 @@ class GildedRose {
             increaseQuality(item);
         }
 
-        if (isExpired(item)) {
+        if (item.sellIn < 0) {
             item.quality = 0;
         }
+
+        item.sellIn--;
     }
 
-    private void updateStandardItemQuality(Item item) {
-        decreaseQuality(item);
-
-        if (isExpired(item)) {
-            decreaseQuality(item);
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
         }
     }
 }
 
-public void updateQuality() {
-    for (Item item : items) {
-        if (isSulfuras(item)) {
-            continue; // Skip Sulfuras items as their quality does not change
+class SulfurasUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
+        // Sulfuras doesn't change in quality or sellIn
+    }
+}
+
+class StandardItemUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
+        decreaseQuality(item);
+        if (item.sellIn < 0) {
+            decreaseQuality(item);
         }
+        item.sellIn--;
+    }
 
-        decreaseSellIn(item);
-
-        if (isAgedBrie(item)) {
-            updateAgedBrieQuality(item);
-            continue;
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
         }
-
-        if (isBackstagePass(item)) {
-            updateBackstagePassQuality(item);
-            continue;
-        }
-
-        updateStandardItemQuality(item);
     }
 }
